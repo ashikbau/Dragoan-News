6cf2fdb53542c9f4dc4b4dea3d9d2f28933cc3d9
Your Name <you@example.com>
2025-05-18 13:47:33 +0200

    added main part

--- src/components/pages/CategoryNews.jsx
+++ src/components/pages/CategoryNews.jsx
@@ -1,16 +1,24 @@
 import { useLoaderData } from "react-router";
+import NewsCards from "../NewsCards";
 
 
 
 const CategoryNews = () => {
-const {data} = useLoaderData();
-console.log(data)
+const {data:news} = useLoaderData();
+
 
     
 
     return (
         <div>
-           <h2>{data.length} News Found on this Category</h2>
+           <h2 className="font-semibold mb-3">Dragoan News Home</h2>
+           <p className="text-gray-400 text-sm">{news.length} News Found on this Category</p>
+           <div>
+            {
+                news.map(singleNews => <NewsCards key={singleNews._id} news={singleNews}></NewsCards>)
+            }
+
+           </div>
         </div>
     );
 };
